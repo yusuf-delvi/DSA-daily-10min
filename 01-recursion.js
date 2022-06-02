da546402dfdc5c3c5a4d42480648d620d863d569
Yusuf-Delvi <yusufmddelvi8@gmail.com>
2022-06-02 23:30:25 +0530

    recursion

---
+++ 01-recursion.js
@@ -0,0 +1,32 @@
+// Problem: SUM 1 to n:
+// N sum problem
+function sum(n) {
+	// 1. Base case
+	if (n === 1) {
+		return 1;
+	}
+
+	// 3. Recursive formula
+	let recResult = sum(n - 1);
+
+	// 2. Small calculation
+	let result = n + recResult;
+
+	return result;
+}
+
+// Problem: FACTORIAL SUM 1 to n:
+// N factorial sum problem
+function factorial(n) {
+	// Base case
+	if (n === 1) {
+		return 1;
+	}
+
+	// Recursive formula
+	// Small calc
+	return n * factorial(n - 1);
+}
+
+console.log(sum(5)); // 15
+console.log(factorial(5)); // 120
